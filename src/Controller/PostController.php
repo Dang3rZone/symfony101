2a0a73978b9a8e6655eaa6ec0f519866225494bc
Andres Rosales <andresrosalespinzon@gmail.com>
2022-01-31 19:39:48 +0100

    feat: views for create and and home

---
+++ src/Controller/PostController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+
+#[Route('/post', name: 'post.')]
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAll();
+
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, PersistenceManagerRegistry $doctrine): Response
+    {
+        // create a new post with title
+        $post = new Post();
+
+        $post->setTitle('this is a title');
+
+        // entity manager
+        $em = $doctrine->getManager();
+
+        $em->persist($post);
+        $em->flush();
+
+        // return a response
+        return new Response('Post created');
+    }
+
+    #[Route('/show/{id}', name: 'show')]
+    public function show(Post $post): Response
+    {
+        // create show view
+        return $this->render('post/show.html.twig', [
+                'post' => $post
+            ]
+        );
+    }
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete(Post $post, PersistenceManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+
+        $em->remove($post);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('post.index'));
+    }
+}
